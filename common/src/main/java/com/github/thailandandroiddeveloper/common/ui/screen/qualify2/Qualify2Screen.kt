5a55ab5b6c5856d102521cce5cdf9710e308d45a
Paul Hochaicharoen <paul.hoc@ascendcorp.com>
2024-01-27 23:02:01 +0700

    done:
    Qualify2 remove imports

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -10,20 +10,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -109,7 +103,9 @@ fun Qualify2Screen() {
             ) {
                 Button(
                     onClick = {},
-                    modifier = Modifier.size(width = 251.dp, height = 40.dp).align(Alignment.Center),
+                    modifier = Modifier
+                        .size(width = 251.dp, height = 40.dp)
+                        .align(Alignment.Center),
                 ) {
                     Text(text = "Next", style = MaterialTheme.typography.labelLarge)
                 }

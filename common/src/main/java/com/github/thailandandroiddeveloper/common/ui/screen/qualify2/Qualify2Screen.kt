8060554b3c024f8ce06a80329ca3208ee3cfa33d
Paul Hochaicharoen <paul.hoc@ascendcorp.com>
2024-01-27 23:01:17 +0700

    done:
    Qualify2

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -1,13 +1,205 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify2
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
+import com.github.thailandandroiddeveloper.common.ui.theme.LightColors
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Qualify2Screen() {
-    // TODO: Qualify2
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                actions = {
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = LightColors.Primary
+                        )
+                    ) {
+                        Text(text = "Skip", style = MaterialTheme.typography.labelLarge)
+                    }
+                }
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            Modifier.padding(it)
+        ) {
+            Box {
+                Column(
+                    modifier = Modifier.padding(horizontal = 32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        text = "Lorem ipsum dolor sit amet",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Text(
+                        modifier = Modifier.padding(bottom = 32.dp),
+                        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dictum lacinia. Integer arcu neque, porttitor ac metus quis, iaculis molestie magna. Vivamus molestie justo sed nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center
+                    )
+                    Image(
+                        modifier = Modifier.padding(bottom = 32.dp),
+                        painter = painterResource(id = R.drawable.img_qualify_2_onboard),
+                        contentDescription = ""
+                    )
+                    PageIndicator(
+                        numberOfPages = 5,
+                        selectedPage = 3,
+                        defaultColor = LightColors.PrimaryContainer,
+                        selectedColor = LightColors.Primary,
+                        defaultRadius = 16.dp,
+                        space = 8.dp,
+                        selectedLength = 32.dp
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .padding(top = 66.dp)
+                    .fillMaxSize()
+                    .background(color = LightColors.PrimaryContainer)
+            ) {
+                Button(
+                    onClick = {},
+                    modifier = Modifier.size(width = 251.dp, height = 40.dp).align(Alignment.Center),
+                ) {
+                    Text(text = "Next", style = MaterialTheme.typography.labelLarge)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PageIndicatorView(
+    isSelected: Boolean,
+    selectedColor: Color,
+    defaultColor: Color,
+    defaultRadius: Dp,
+    selectedLength: Dp,
+    animationDurationInMillis: Int,
+    modifier: Modifier = Modifier,
+) {
+    val color: Color by animateColorAsState(
+        targetValue = if (isSelected) {
+            selectedColor
+        } else {
+            defaultColor
+        },
+        animationSpec = tween(
+            durationMillis = animationDurationInMillis,
+        ), label = ""
+    )
+    val width: Dp by animateDpAsState(
+        targetValue = if (isSelected) {
+            selectedLength
+        } else {
+            defaultRadius
+        },
+        animationSpec = tween(
+            durationMillis = animationDurationInMillis,
+        ), label = ""
+    )
+
+    Canvas(
+        modifier = modifier
+            .size(
+                width = width,
+                height = defaultRadius,
+            ),
+    ) {
+        drawRoundRect(
+            color = color,
+            topLeft = Offset.Zero,
+            size = Size(
+                width = width.toPx(),
+                height = defaultRadius.toPx(),
+            ),
+            cornerRadius = CornerRadius(
+                x = defaultRadius.toPx(),
+                y = defaultRadius.toPx(),
+            ),
+        )
+    }
+}
+
+@Composable
+fun PageIndicator(
+    numberOfPages: Int,
+    modifier: Modifier = Modifier,
+    selectedPage: Int = 0,
+    selectedColor: Color = Color(0xFF3E6383),
+    defaultColor: Color = Color.LightGray,
+    defaultRadius: Dp = 20.dp,
+    selectedLength: Dp = 60.dp,
+    space: Dp = 30.dp,
+    animationDurationInMillis: Int = 300,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(space),
+        modifier = modifier,
+    ) {
+        for (i in 0 until numberOfPages) {
+            val isSelected = i == selectedPage
+            PageIndicatorView(
+                isSelected = isSelected,
+                selectedColor = selectedColor,
+                defaultColor = defaultColor,
+                defaultRadius = defaultRadius,
+                selectedLength = selectedLength,
+                animationDurationInMillis = animationDurationInMillis,
+            )
+        }
+    }
 }
 
 // region Read-only because we use this to process your score.

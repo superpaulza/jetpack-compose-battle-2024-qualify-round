0964735e4913e9eb08368ab5befa59dba293efed
Paul Hochaicharoen <paul.hoc@ascendcorp.com>
2024-01-27 23:59:24 +0700

    done:
    3

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify3/Qualify3Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify3/Qualify3Screen.kt
@@ -1,13 +1,124 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify3
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
+import com.github.thailandandroiddeveloper.common.ui.theme.LightColors
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Qualify3Screen() {
-    // TODO: Qualify3
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                        Text(text = "John Doe",
+                            style = MaterialTheme.typography.titleLarge,
+                            color = LightColors.OnPrimaryContainer)
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(id = R.drawable.ic_qualify_3_back),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(id = R.drawable.ic_qualify_3_calendar),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(id = R.drawable.ic_qualify_3_copy),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            modifier = Modifier.size(48.dp),
+                            painter = painterResource(id = R.drawable.ic_qualify_3_menu),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+            )
+        },
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(it)
+        ) {
+            val scrollState = rememberScrollState()
+            Row(
+                modifier = Modifier.horizontalScroll(scrollState)
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(shape = RoundedCornerShape(16.dp)),
+                    painter = painterResource(id = R.drawable.img_qualify_3_photo_1),
+                    contentDescription = "Localized description"
+                )
+                Image(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(shape = RoundedCornerShape(16.dp)),
+                    painter = painterResource(id = R.drawable.img_qualify_3_photo_2),
+                    contentDescription = "Localized description"
+                )
+                Image(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(shape = RoundedCornerShape(16.dp)),
+                    painter = painterResource(id = R.drawable.img_qualify_3_photo_3),
+                    contentDescription = "Localized description"
+                )
+            }
+        }
+    }
 }
 
 // region Read-only because we use this to process your score.
